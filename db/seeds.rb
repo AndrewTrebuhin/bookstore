e12a68bc676aa92606dc340a3b5e2383b7647174
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2014-08-18 22:47:38 +0300

    Bike-store scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.delete_all
+
+Product.create!(title: 'Programming Ruby 2.0',
+                description: 'This is book.',
+                image_url: 'ruby_book.jpg',
+                price: 49.95)
