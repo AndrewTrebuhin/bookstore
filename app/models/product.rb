9fb1dee051349839026ed076bba082e5708125ef
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2014-08-19 23:09:28 +0300

    Add validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true, length: { minimum: 5 }
+  validates :image_url, allow_blank: true, format: {
+            with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+            message: 'URL must link to GIF, JPG, JPEG or PNG image.' }
 end
